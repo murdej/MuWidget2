dc0b458853b2619670d3ba43739cc19c3914ddf7
Vit Peprnicek <vit.peprnicek@nwt.cz>
2022-05-22 15:45:02 +0200

    ...

--- dist/MuWidget-bundle.js
+++ dist/MuWidget-bundle.js
@@ -703,7 +703,25 @@ class MuWidget {
     }
     muBindList(list, templateName, container, commonParams = null, finalCalback = null) {
     }
-    muVisible(state, control) { }
+    muVisible(state, control) {
+        if (Array.isArray(control)) {
+            for (const controlItem of control)
+                this.muVisible(state, controlItem);
+        }
+        else {
+            let neg = false;
+            if (typeof control === 'string') {
+                if (control.startsWith('!')) {
+                    neg = true;
+                    control = control.substring(1);
+                }
+                if (!(control in this.ui))
+                    throw new Error("Control with mu-id='" + control + "' not found.");
+                control = this.ui[control];
+            }
+            control.style.display = state !== neg ? null : "none";
+        }
+    }
     muBindData(srcData) {
         MuBinder.bindData(this.muBindOpts, srcData, this);
         this.muAfterBindData();
